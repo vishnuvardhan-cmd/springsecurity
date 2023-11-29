2137a33a3c5e8e1d65a06f5b4754c1e9fd3ad892
vishnu <bvishnu123vardhan@gmail.com>
2023-11-29 20:29:06 +0530

    intial commit

---
+++ spring-security-db/src/main/java/com/dailycodelearner/SpringSecurityDbApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodelearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDbApplication.class, args);
+	}
+
+}
